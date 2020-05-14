4affaeb63adc1c6b4d13c5ee3e200586616c127d
Dima Goltsman <dimitrig@wix.com>
2020-05-14 16:22:26 +0300

    .

--- dist/generic-remote-content-card.js
+++ dist/generic-remote-content-card.js
@@ -1,16 +1,16 @@
  
 function loadScript(remote_template){
-	if(window[`scriptLoaded_${remote_template}`]){
-		return;
-	}
+  if(window[`scriptLoaded_${remote_template}`]){
+    return;
+  }
 
-	var script = document.createElement("script");
-	script.src = `/hacsfiles/generic-remote-control-card/remotes/${remote_template}/remote-html.js`;
-	script.type = "text/javascript";
-	script.async = false;
-	document.head.appendChild(script); 
-	window[`scriptLoaded_${remote_template}`] = true;
-	
+  var script = document.createElement("script");
+  script.src = `/hacsfiles/generic-remote-control-card/remotes/${remote_template}/remote-html.js`;
+  script.type = "text/javascript";
+  script.async = false;
+  document.head.appendChild(script); 
+  window[`scriptLoaded_${remote_template}`] = true;
+  
 }
 
 
@@ -21,7 +21,7 @@ class GenericRemotControlCard extends HTMLElement {
     this.attachShadow({ mode: 'open' });
   }
   setConfig(config) {
-  	loadScript(config.remote_template);
+    loadScript(config.remote_template);
     const root = this.shadowRoot;
     if (root.lastChild) root.removeChild(root.lastChild);
 
@@ -31,7 +31,7 @@ class GenericRemotControlCard extends HTMLElement {
 
   
   set hass(hass) {
-  	
+    
 
   
     const config = this._config;
@@ -39,7 +39,7 @@ class GenericRemotControlCard extends HTMLElement {
     loadScript(config.remote_template);
     
     try{
-    	const html = window[`getRemoteHtml_${config.remote_template}`](config);
+      const html = window[`getRemoteHtml_${config.remote_template}`](config);
         const css = window[`getRemoteStyle_${config.remote_template}`](config);
         
         
@@ -62,24 +62,28 @@ class GenericRemotControlCard extends HTMLElement {
         }
     
     } catch(err){
-    	console.log('waiting for remote load');
-    	loadScript(config.remote_template);
+      console.log('waiting for remote load');
+      loadScript(config.remote_template);
     }
     
   }
   
-
+ 
     
   _bindButtons(card, hass, config){
-  	var buttons =  card.getElementsByClassName(`myButton-${config.remote_template}`);
+    var buttons =  card.getElementsByClassName(`myButton-${config.remote_template}`);
     var i;
     for (i = 0; i < buttons.length; i++) { 
-    	let button = buttons[i]
+      let button = buttons[i]
         button.addEventListener('click', function(source){
-        	console.log(button.id);
-        	hass.callService('broadlink',
-        	                  'send', 
-        	                  {packet: [getPacket(button.id, config)], host: config.broadlink_host}
+          console.log(button.id);
+          let buttonData = getButtonData(button.id, config);
+          console.log(buttonData)
+          let domain = buttonData.call.split(".")[0]
+          let action = buttonData.call.split(".")[1]
+          hass.callService(domain,
+                            action, 
+                            buttonData.data
                              );
         });
     }
@@ -95,8 +99,8 @@ class GenericRemotControlCard extends HTMLElement {
 
 }
 
- function getPacket(buttonId, config){
-  	return config.buttons[buttonId];
+ function getButtonData(buttonId, config){
+    return config.buttons[buttonId];
   }
 
 customElements.define('generic-remote-control-card', GenericRemotControlCard);
