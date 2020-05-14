b360e34b0ff0154c87a9445331c9d527c372ecda
Dima Goltsman <dimitrig@wix.com>
2020-05-14 15:51:16 +0300

    .

--- dist/generic-remote-content-card.js
+++ dist/generic-remote-content-card.js
@@ -5,7 +5,7 @@ function loadScript(remote_template){
 	}
 
 	var script = document.createElement("script");
-	script.src = `remotes/${remote_template}/remote-html.js`;
+	script.src = `/hacsfiles/generic-remote-control-card/remotes/${remote_template}/remote-html.js`;
 	script.type = "text/javascript";
 	script.async = false;
 	document.head.appendChild(script); 
