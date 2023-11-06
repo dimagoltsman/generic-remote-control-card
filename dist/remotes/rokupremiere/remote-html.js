09cee116facdabbe4ec560161446490bdfc6d138
stain3565 <60330351+stain3565@users.noreply.github.com>
2023-11-06 15:25:46 +0000

    Better images
    Improved quality of remote image and buttons, and reflected these in remote-html javascript

--- dist/remotes/rokupremiere/remote-html.js
+++ dist/remotes/rokupremiere/remote-html.js
@@ -9,26 +9,20 @@ function getRemoteStyle_rokupremiere(config) {
     }
     body {
 	margin: 0;
-	padding: 20px;
+	padding: 0px;
 	font-family: "Helvetica Neue", Helvetica, Arial, Geneva, sans-serif
 }
 
 .container {
-	width: 220px;
+	width: 242px;
 	margin: 0 auto
 }
 
 #remote-control-rokupremiere {
 	position: relative;
 	background: url(${base_url}/rokupremiere.png?1) no-repeat;
-	width: 191px;
-	height: 578px
-}
-
-#remote-control-rokupremiere h2,
-#remote-control-rokupremiere span {
-	position: absolute;
-	left: 200px
+	width: 242px;
+	height: 720px
 }
 
 #remote-control-rokupremiere ul {
@@ -59,130 +53,130 @@ function getRemoteStyle_rokupremiere(config) {
 }
 
 #remote-control-rokupremiere li#rewind a {
-    left: 30px;
-    top: 269px;
-    width: 35px;
-    height: 35px;	
+    left: 54px;
+    top: 369px;
+    width: 30px;
+    height: 18px;	
 	background: url(${base_url}/rewind.png?1) no-repeat
 }
 
 #remote-control-rokupremiere li#forward a {
-    left: 129px;
-    top: 267px;
+    left: 157px;
+    top: 369px;
     width: 30px;
-    height: 39px;	
+    height: 18px;	
 	background: url(${base_url}/forward.png?1) no-repeat
 }
 
 #remote-control-rokupremiere li#playpause a {
-    left: 68px;
-    top: 269px;
-    width: 58px;
-    height: 38px;	
+    left: 109px;
+    top: 370px;
+    width: 24px;
+    height: 18px;	
 	background: url(${base_url}/playpause.png?1) no-repeat
 }
 
 #remote-control-rokupremiere li#left a {
-	left: 51px;
-    top: 159px;
-    width: 19px;
-    height: 21px;	
+	left: 54px;
+    top: 207px;
+    width: 24px;
+    height: 27px;	
 	background: url(${base_url}/left.png?1) no-repeat
 }
 
 #remote-control-rokupremiere li#right a {
-    left: 125px;
-    top: 159px;
-    width: 18px;
-    height: 22px;	
+    left: 157px;
+    top: 207px;
+    width: 24px;
+    height: 27px;	
 	background: url(${base_url}/right.png?1) no-repeat
 }
 
 #remote-control-rokupremiere li#up a {
-    left: 86px;
-    top: 121px;
-    width: 22px;
-    height: 21px;	
+    left: 102px;
+    top: 162px;
+    width: 37px;
+    height: 18px;	
 	background: url(${base_url}/up.png?1) no-repeat
 }
 
 #remote-control-rokupremiere li#down a {
-    left: 87px;
-    top: 195px;
-    width: 21px;
-    height: 19px;	
+    left: 102px;
+    top: 261px;
+    width: 37px;
+    height: 18px;	
 	background: url(${base_url}/down.png?1) no-repeat
 }
 
 #remote-control-rokupremiere li#ok a {
-    left: 85px;
-    top: 161px;
-    width: 25px;
-    height: 19px;	
+    left: 102px;
+    top: 207px;
+    width: 37px;
+    height: 27px;	
 	background: url(${base_url}/ok.png?1) no-repeat
 }
 
 #remote-control-rokupremiere li#asterisk a {
-    left: 99px;
-    top: 235px;
-    width: 57px;
-    height: 30px;	
+    left: 145px;
+    top: 315px;
+    width: 30px;
+    height: 27px;	
 	background: url(${base_url}/asterisk.png?1) no-repeat
 }
 
 #remote-control-rokupremiere li#rakuten a {
-    left: 35px;
-    top: 354px;
-    width: 62px;
-    height: 40px;	
+    left: 48px;
+    top: 483px;
+    width: 64px;
+    height: 33px;	
 	background: url(${base_url}/rakuten.png?1) no-repeat
 }
 
 #remote-control-rokupremiere li#spotify a {
-    left: 96px;
-    top: 354px;
-    width: 62px;
-    height: 40px;	
+    left: 127px;
+    top: 483px;
+    width: 64px;
+    height: 32px;	
 	background: url(${base_url}/spotify.png?1) no-repeat
 }
 
 #remote-control-rokupremiere li#googleplay a {
-	left: 96px;
-	top: 316px;
-    width: 62px;
-    height: 40px;	
+	left: 127px;
+	top: 434px;
+    width: 65px;
+    height: 34px;	
 	background: url(${base_url}/googleplay.png?1) no-repeat 0 0px
 }
 
 #remote-control-rokupremiere li#netflix a {
-	left: 34px;
-	top: 316px;
-    width: 62px;
-    height: 40px;	
+	left: 48px;
+	top: 434px;
+    width: 64px;
+    height: 34px;	
 	background: url(${base_url}/netflix.png?1) no-repeat 0 0px
 }
 
 #remote-control-rokupremiere li#leftarrow a {
-    left: 27px;
-    top: 63px;
-    width: 70px;
-    height: 40px;	
+    left: 66px;
+    top: 99px;
+    width: 30px;
+    height: 27px;	
 	background: url(${base_url}/leftarrow.png?1) no-repeat
 }
 
 #remote-control-rokupremiere li#home a {
-    left: 96px;
-    top: 63px;
-    width: 66px;
-    height: 40px;	
+    left: 145px;
+    top: 99px;
+    width: 30px;
+    height: 27px;	
 	background: url(${base_url}/home.png?1) no-repeat
 }
 
 #remote-control-rokupremiere li#replay a {
-    left: 34px;
-    top: 234px;
-    width: 62px;
-    height: 29px;	
+    left: 67px;
+    top: 315px;
+    width: 30px;
+    height: 27px;	
 	background: url(${base_url}/replay.png?1) no-repeat
 }
 
@@ -268,9 +262,6 @@ function getRemoteHtml_rokupremiere(config){
 			<h1>${config.name}</h1>
 			</div>
 			<div id="remote-control-rokupremiere">
-				<h2>
-					Main navigation
-				</h2>
 				<ul>
 					<li class="myButton-${template}" id="playpause"><a href="#" title="playpause"></a></li>
 					<li class="myButton-${template}" id="rewind"><a href="#" title="rewind"></a></li>
